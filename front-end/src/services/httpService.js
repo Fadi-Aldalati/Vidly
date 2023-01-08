776ee0103de2c39ccfd2ea2d8f5d860a4adebd00
Fadi Aldalati <108523626+Fadi-Aldalati@users.noreply.github.com>
2023-01-08 10:54:57 +0300

    Update httpService.js

--- front-end/src/services/httpService.js
+++ front-end/src/services/httpService.js
@@ -21,10 +21,11 @@ function setJwt(jwt){
 axios.defaults.headers.common['x-auth-token']= jwt;
 }
 
-export default {
+const http = {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
   setJwt
 };
+export default http;
